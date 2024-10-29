6319b58c5483d990aec2d54a0be7aac048d69936
Kacpi476 <kapiszixd2@gmail.com>
2024-10-29 19:45:14 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,29 +2,79 @@ namespace Simulator
 {
     internal class Creature
     {
-        private string name;
-        public string Name {
-            get {return name; }
-            set {name = value;}
+        private int level = 1;
+        private string name = "Unknown";
+
+        public string Name
+        {
+            get => name;
+            init
+            {
+                string processedName = value.Trim();
+
+                if (processedName.Length < 3)
+                {
+                    processedName = processedName.PadRight(3, '#');
+                }
+                else if (processedName.Length > 25)
+                {
+                    processedName = processedName.Substring(0, 25).TrimEnd();
+                    if (processedName.Length < 3)
+                    {
+                        processedName = processedName.PadRight(3, '#');
+                    }
+                }
+
+                name = char.ToUpper(processedName[0]) + processedName.Substring(1);
+            }
         }
-        private int level;
-        public int Level {
-            get {return level; }
-            set {level = value;}
+
+        public int Level
+        {
+            get => level;
+            init
+            {
+                if (value < 1)
+                {
+                    value = 1;
+                }
+                else if (value > 10)
+                {
+                    value = 10;
+                }
+                else
+                {
+                    level = value;
+                }
+
+            }
         }
-        public Creature(){ }
+        public Creature() { }
         public Creature(string name, int level = 1)
         {
             Name = name;
-            Level = level;
+            Level = level >= 1 ? level : 1;
         }
+
         public void SayHi()
         {
             Console.WriteLine($"Hi I'm {Name} and my level is {Level}");
         }
+        public void Upgrade()
+        {
+            if (level < 10)
+            {
+                level++;
+            }
+
+        }
+
         public string Info
         {
             get { return $"{Name} [{Level}]"; }
         }
     }
 }
+
+
+
--- Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
+++ Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d419eb284252e27236090763977b06a8bf0aef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e6215565bbdd3bc361a6ac074d10d91193c307")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
